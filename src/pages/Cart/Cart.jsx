e0fe92fc04b1b29ad6d270d2d897f48c3e8ad178
Monu pal <76943034+monupal057@users.noreply.github.com>
2024-05-27 21:04:19 +0530

    Update Cart.jsx

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,13 +1,143 @@
+import  { useEffect, useState } from 'react'
+import Layout from '../Layout/Layout'
+import axios from "axios";
+import login from "../../assets/login.png";
 
-// import React from 'react'
-import Layout from "../../components/Layout/Layout"
-import { Link } from "react-router-dom";
-const Cart = () => {
+const AllProducts = () => {
+
+  const [allProducts, setAllProducts] = useState([])
+  const [originalProducts, setOriginalProducts] = useState([])
+  const [allCategory, setAllCategory] = useState([])
+  const [selectProducts, setSelectProducts] =useState("")
+
+// AllProducts
+  useEffect(()=>{
+    const AllProducts = async () => {
+    const res = await axios ("https://dummyjson.com/products")
+    setAllProducts(res.data.products)
+    setOriginalProducts(res.data.products)
+    }
+
+    AllProducts();
+  },[])
+
+  // product category
+  useEffect(()=> {
+    const getAllProductsCategory = async () => {
+      try{
+        const res = await axios("https://dummyjson.com/products/categories");
+        setAllCategory(res.data);
+      }catch(error){
+        console.log(error)
+      }
+    };
+    getAllProductsCategory();
+  },[])
+  console.log(allCategory)
+
+  const filterProducts = async(selectcategory) => {
+    setSelectProducts(selectcategory);
+    setSelectProducts(selectcategory);
+    let category = selectcategory.split(" ").join("-").toLowerCase();
+    console.log(category);
+    const json = await axios.get(`https://dummyjson.com/products/category/${category}`);
+    console.log(json);
+    
+    setAllProducts(json.data.products);
+  }
+
+
+  //   const data = selectcategory? originalProducts.filter((filterItem) =>filterItem.category === selectcategory.toLowerCase() )
+  //    :originalProducts
+  //   setAllProducts(data)
+ 
+  //  }
+  // //  console.log()
+
+   
+
   return (
+    <>
     <Layout>
-      <Link to='/cart'>Cart</Link>
-      </Layout>
-  );
-};
+       <div className="relative">
+        <img
+        src={login}
+        alt=""
+        className=" object-cover w-full object-center h-[200px] mt-5"/>
+        <div className="w-full h-[200px] bg-black absolute top-0 left-0 opacity-[.4]"></div>
+        <h2 className=" absolute top-[40%] left-[10%] text-white font-semibold text-3xl md:text-5xl">
+          All Products
+        </h2>
+      </div>
 
-export default Cart;
+      {/* products category section */}
+     <div className=" flex gap-3 flex-wrap ">
+      <select onChange={(e) => filterProducts(e.target.value)}>
+        <option>
+          Filter by Category
+        </option>
+      
+
+
+
+
+      {
+        allCategory
+        .filter(
+          (filterItem) => !["lighting", "motorcycle", "automotive", "furniture"]
+          .includes(filterItem)
+        )
+        .map((item, index) =>(
+          
+          // eslint-disable-next-line react/jsx-key
+          <option value={item.name}>
+          {item.name}
+            </option>
+          
+           
+        ))
+      }
+      {/* {
+        allCategory.map((item , index) => {
+          return (
+            <option value={item}>
+          {item}
+            </option>
+          )
+          
+        })
+      } */}
+      </select>
+      </div>
+
+  {/* All product */}
+     
+     <div className="flex gap-4 flex-wrap justify-center">
+      {allProducts.map((AllItems, index)=>(
+          <div key={index} className="border">
+            <img src={AllItems.thumbnail} alt="" 
+              className="object-cover object-center  block"/>
+            <div className="mt-4">
+             <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+               Title: {AllItems.title}
+              </h3>
+             <h2 className="text-gray-900 title-font text-lg font-medium">
+               Rating : {AllItems.rating}
+              </h2>
+             <p className="mt-1">
+              Price:  {AllItems.price} Rs.</p>
+           </div>
+           <p className="mt-1">
+              Price:  {AllItems.price} Rs.</p>
+            </div>
+        ))
+      }
+     </div>
+
+    
+    </Layout>
+    </>
+  )
+}
+
+export default AllProducts;
